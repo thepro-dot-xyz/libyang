c015832384b9b88cb0e353ae97821ccf262d9725
Kunihiro Ishiguro <ishi@hash-set.com>
2020-07-28 05:01:20 +0900

    Add scan_dir() test.

---
+++ tests/scan_dir.rs
@@ -0,0 +1,14 @@
+use libyang::Yang;
+use std::path::PathBuf;
+
+#[test]
+fn scan_dir() {
+    let yang = Yang::new();
+    let path = PathBuf::from("tests/ietf-dhcp@2016-08-25.yang");
+
+    if let Ok(p) = yang.scan_dir("tests", "ietf-dhcp.yang", false) {
+        assert_eq!(p, path);
+    } else {
+        panic!("scan_dir should match to yang file.");
+    }
+}
