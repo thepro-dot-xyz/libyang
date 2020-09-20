6b0feef7c31a0377ab3841f0b9e0f46ed3f66348
Kunihiro Ishiguro <ishi@hash-set.com>
2020-09-20 06:00:46 -0700

    Try to parse "iana-if-type.yang".

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,14 @@ fn main() {
 
     // Read a module.
     let mut ms = Modules::new();
-    let yang_name = "ietf-interfaces";
+    let yang_name = "iana-if-type";
     let data = yang.read(&ms, yang_name).unwrap();
 
     match yang_parse(&data) {
         Ok((_, module)) => {
             ms.modules.insert(module.prefix.to_owned(), module);
 
-            let entry = ms.modules.get(&"if".to_string());
+            let entry = ms.modules.get(&"ianaift".to_string());
             if let Some(e) = entry {
                 println!("Module found");
                 println!("name: {}", e.name);
