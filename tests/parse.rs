b839220460658368872140b28f098a251c07eff8
Kunihiro Ishiguro <ishi@hash-set.com>
2020-09-20 05:55:56 -0700

    Test for parsing "ietf-interfaces.yang".

--- tests/parse.rs
+++ tests/parse.rs
@@ -55,3 +55,31 @@ fn parse_ietf_yang_types_test() {
         }
     }
 }
+
+#[test]
+fn parse_ietf_interfaces_test() {
+    let mut yang = Yang::new();
+    yang.add_path("/etc/openconfigd/yang:yang/...");
+
+    // Read a module.
+    let mut ms = Modules::new();
+    let yang_name = "ietf-interfaces";
+    let data = yang.read(&ms, yang_name).unwrap();
+
+    match yang_parse(&data) {
+        Ok((_, module)) => {
+            ms.modules.insert(module.prefix.to_owned(), module);
+
+            let entry = ms.modules.get(&"if".to_string());
+            if let Some(_) = entry {
+                // Success.
+            } else {
+                // Module not found.
+                panic!("modules can't find")
+            }
+        }
+        Err(e) => {
+            panic!("module parse error: {:?}", e);
+        }
+    }
+}
